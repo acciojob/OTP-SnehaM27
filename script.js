f59441bf21942e4d1b053a1448e6348f5c2d961d
SnehaM27 <snehamohan0227@gmail.com>
2025-11-17 19:24:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-//your JS code here. If required.
 const inputs = document.querySelectorAll(".code");
 
+// Focus first box initially
 inputs[0].focus();
 
 inputs.forEach((input, index) => {
+
     input.addEventListener("input", () => {
-        input.value = input.value.replace(/[^0-9]/g, ""); // allow only digits
+        input.value = input.value.replace(/[^0-9]/g, "");
 
         if (input.value && index < inputs.length - 1) {
             inputs[index + 1].focus();
@@ -13,10 +14,17 @@ inputs.forEach((input, index) => {
     });
 
     input.addEventListener("keydown", (e) => {
+
+        // BACKSPACE behavior
         if (e.key === "Backspace") {
-            if (input.value === "" && index > 0) {
-                inputs[index - 1].value = "";
-                inputs[index - 1].focus();
+            if (input.value === "") {
+                if (index > 0) {
+                    inputs[index - 1].value = "";
+                    inputs[index - 1].focus();
+                }
+            } else {
+                // If there is content, delete it but DO NOT jump immediately
+                input.value = "";
             }
         }
     });
